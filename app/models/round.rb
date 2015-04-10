dfc467c8b732c2dbe6cd2449a5538f42c942f503
Alexei <amihalop@gmail.com>
2015-04-10 16:05:04 -0400

    Write model file for round class

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,3 +1,7 @@
 class Round < ActiveRecord::Base
-  # Remember to create a migration!
+  belongs_to :user
+  belongs_to :deck
+
+  validates :num_incorrect, :num_correct,
+            :user_id, :deck_id, presence: true
 end
