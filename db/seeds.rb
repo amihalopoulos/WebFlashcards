7c3a8862bab28170b7e44691c6910cd2eaddd1e8
suprfrye <tara.c.frye@gmail.com>
2015-04-10 17:41:46 -0400

    Add users, deck, and cards

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+joe = User.create(name: 'Joe', password: '1234')
+lisa = User.create(name: 'Lisa', password: '1234')
+
+deck = Deck.create(topic: 'Arabic', user: joe)
+
+Card.create(term: 'hello', description: 'marhaba', deck: deck)
+Card.create(term: 'welcome', description: 'ahlan wa sahlan', deck: deck)
+Card.create(term: 'student', description: 'al jameat', deck: deck)
