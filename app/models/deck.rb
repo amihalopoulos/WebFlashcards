eb624dcc433de4ac0f799176eeb35771d5a4fd33
Ben Morimoto <bkmorimoto@gmail.com>
2015-04-10 15:00:25 -0400

    add class method to create a deck with a csv file

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -2,4 +2,17 @@ class Deck < ActiveRecord::Base
   has_many :cards
   has_many :rounds
   has_many :users, through: :rounds
+
+  def self.create_new_deck(args)
+    deck = Deck.create(name: args[:name], description: args[:description])
+    File.open(args[:path]) do |f|
+      f.each_line do |line|
+        array = line.chomp.split(',')
+        question = array[0].to_s.downcase
+        answer = array[1].to_s.downcase
+        Card.create(question: question, answer: answer, deck: deck)
+      end
+    end
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,8 @@
-require 'csv'
 
-state_capitals_path = File.expand_path('../seed_data/states.csv', __FILE__)
+state_capital_hash = {
+  name: "State Capitals",
+  description: "Learn the state capitals",
+  path: File.expand_path('../seed_data/states.csv', __FILE__)
+}
 
-state_capitals_deck = Deck.create(name: 'State Capitals', description: "Learn the state capitals")
-File.open(state_capitals_path) do |f|
-  f.each_line do |line|
-    array = line.chomp.split(',')
-    question = array[0].to_s.downcase
-    answer = array[1].to_s.downcase
-    Card.create(question: question, answer: answer, deck: state_capitals_deck)
-  end
-end
+Deck.create_new_deck(state_capital_hash)
