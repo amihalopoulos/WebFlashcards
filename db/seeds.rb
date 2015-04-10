3fd28e758f6cb8709b31981cc1b2c3e733853220
Ben Morimoto <bkmorimoto@gmail.com>
2015-04-10 15:11:47 -0400

    add users to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+require 'faker'
+
+index = 0
+10.times do
+  index += 1
+  User.create(name: Faker::Name.name, email: "#{index}@email.com", password: "123")
+end
 
 state_capital_hash = {
   name: "State Capitals",
@@ -6,3 +13,11 @@ state_capital_hash = {
 }
 
 Deck.create_new_deck(state_capital_hash)
+
+world_capital_hash = {
+  name: "World Capitals",
+  description: "Learn the capitals from around the world",
+  path: File.expand_path('../seed_data/country-capitals.csv', __FILE__)
+}
+
+Deck.create_new_deck(world_capital_hash)
