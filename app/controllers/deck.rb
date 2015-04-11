7bf9b16404ca4a7cf3329b13e75d2157da3e6c13
Alexander Mihalopoulos <amihalop@gmail.com>
2015-04-11 08:45:24 -0400

    Add ability to create new deck

--- app/controllers/deck.rb
+++ app/controllers/deck.rb
@@ -1,8 +1,22 @@
-get '/deck/:id' do |id|
-  @deck = Deck.find(id) #find deck of current user with id
-  @author = users_deck?(@deck) ? true : false
-    #show with edits/delete
-  #else
-    #show as is
+get '/deck/new' do
+  if current_user
+    erb :'/deck/new'
+  else
+    flash[:error] = "You must log in to perform that action!"
+    redirect '/'
+  end
+end
+post '/deck/new' do
+  user = User.find(session[:user_id])
+  @deck = user.decks.create(params[:deck])
+
   erb :'/deck/show'
 end
+get '/deck/:id' do |id|
+  @deck = Deck.find(id)
+  @author = users_deck?(@deck) ? true : false
+  erb :'/deck/show'
+end
+
+
+
