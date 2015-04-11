3adcb171d205b8f4590557349bf1fc9cb23b4170
Dan Asselin <dasselin86@gmail.com>
2015-04-11 18:26:44 -0400

    Create #foreign_decks custom method in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,11 @@ class User < ActiveRecord::Base
   has_many :played_decks, through: :rounds, source: :deck
 
   validates :name, :password_digest, presence: true
+
+  def foreign_decks
+    Deck.all.select do |deck|
+      deck if deck.user_id != self.id
+    end
+  end
+
 end
