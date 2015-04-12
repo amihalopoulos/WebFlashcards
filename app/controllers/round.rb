2da61b7193a2454982d2a07ded3595b0073c83da
suprfrye <tara.c.frye@gmail.com>
2015-04-12 16:58:26 -0400

    Use session to save cards id

--- app/controllers/round.rb
+++ app/controllers/round.rb
@@ -3,13 +3,16 @@ get '/rounds/:id' do |id|
   @user = User.find(session[:user_id])
   @deck = @round.deck
   cards = @deck.cards
-  if params[:cards]
-    @cards = params[:cards]
+  if session[:cards]
+    redirect "/rounds/#{@round.id}/stats" unless session[:cards].length > 0
+    @cards = session[:cards]
   else
-    @cards = @deck.shuffle(cards)
-    params[:cards] = @cards
+    @cards = @deck.shuffle(cards).map do |card|
+      card.id
+    end
+    session[:cards] = @cards
   end
-  @cur_card = current_card(@cards)
+  @cur_card = Card.find_by(current_card(@cards))
   if @cur_card
     erb :'round/show'
   else
@@ -22,5 +25,6 @@ post '/rounds' do
   deck = Deck.find(params[:deck_id])
   user = User.find(session[:user_id])
   round = Round.create(deck: deck, user: user)
+  session[:cards] = nil
   redirect "/rounds/#{round.id}"
 end
