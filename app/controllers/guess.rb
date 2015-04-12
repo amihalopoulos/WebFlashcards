bb56ba0443551d7259cb9286f69c59dac35e6438
suprfrye <tara.c.frye@gmail.com>
2015-04-12 14:22:00 -0400

    Add guess_correct? and update_round methods

--- app/controllers/guess.rb
+++ app/controllers/guess.rb
@@ -1,8 +1,9 @@
 post '/guesses' do
-  guess = Guess.new(params[:guess])
-  if guess.save
-    redirect "/round/#{guess.round_id}"
-  else
-    redirect back
-  end
+  guess = Guess.create(params[:guess])
+  # update round incorrect/correct
+  round = Round.find(guess.round_id)
+  card = guess.card
+  guess.update_round(card, round)
+  puts "This is after update in guess #{guess.round.num_correct}, #{guess.round.num_incorrect}"
+  redirect "/rounds/#{guess.round_id}"
 end
