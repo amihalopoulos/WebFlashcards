759f01ae599ae3057f3bac783d25ee8731144342
suprfrye <tara.c.frye@gmail.com>
2015-04-12 14:25:31 -0400

    Update get rounds to check state of variables

--- app/controllers/round.rb
+++ app/controllers/round.rb
@@ -1,12 +1,23 @@
 get '/rounds/:id' do |id|
   @round = Round.find(id)
+  @user = User.find(session[:user_id])
   # Temporary Deck retrieval method
   # @deck = Deck.find(params[:deck_id])
   @deck = Deck.first
   cards = @deck.cards
-  @cards = @deck.shuffle(cards)
-  @user = current_user
-  erb :'round/show'
+  if params[:cards]
+    @cards = params[:cards]
+  else
+    @cards = @deck.shuffle(cards)
+    params[:cards] = @cards
+  end
+  @cur_card = current_card(@cards)
+  if @cur_card
+    erb :'round/show'
+  else
+    redirect "/rounds/#{@round.id}/stats"
+    # erb :'round/stats'
+  end
 end
 
 post '/rounds' do
