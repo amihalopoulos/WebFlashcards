99cc2f19d68787423e593dfb2fd2e8e23aac90c9
suprfrye <tara.c.frye@gmail.com>
2015-04-12 14:24:35 -0400

    Add call to update round

--- app/controllers/guess.rb
+++ app/controllers/guess.rb
@@ -4,6 +4,5 @@ post '/guesses' do
   round = Round.find(guess.round_id)
   card = guess.card
   guess.update_round(card, round)
-  puts "This is after update in guess #{guess.round.num_correct}, #{guess.round.num_incorrect}"
   redirect "/rounds/#{guess.round_id}"
 end
